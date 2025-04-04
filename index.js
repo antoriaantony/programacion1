bd89de180ad50d7d92987b73d01156d3eabb9799
antoriaantony <antonyantoria@gmail.com>
2025-04-03 21:27:33 -0300

    guardado index

---
+++ index.js
@@ -0,0 +1,23 @@
+let usuario={
+    nombre: "Antony",
+    apellido: "Antoria",
+    edad: 19,
+    ci: 463266890,
+    calificacion:3
+ } //objeto
+ console.log(usuario)
+ /*
+let nombre = "Antony" //variable
+let apellido = "Antoria"
+let edad =19
+let ci=463266890
+let calificacion=3
+if(calificacion >=7) { console.log ("aprobado")
+} 
+
+else {
+    console.log ("reprobado")
+}
+    
+console.log(nombre,apellido,edad,ci,calificacion) // Mostrar la variable
+*/
